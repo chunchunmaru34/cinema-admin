422bb3503491681a52b42db65b20f304b6a2e269
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2018-03-12 16:33:19 +0300

    service urls fixes

--- src/app/cinema/seats-type.service.ts
+++ src/app/cinema/seats-type.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SeatsType } from './seats-type';
+import { SEATS_TYPES_URL } from '../constants/api-endpoints';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json'})
@@ -9,20 +10,18 @@ const httpOptions = {
 
 @Injectable()
 export class SeatsTypeService {
-  private seatsTypesUrl = 'http://localhost:3003/seats-types';
-
   constructor(private http: HttpClient) { }
 
   getSeatsTypes(): Observable<SeatsType[]> {
-    return this.http.get<SeatsType[]>(this.seatsTypesUrl);
+    return this.http.get<SeatsType[]>(SEATS_TYPES_URL);
   }
 
   getSeatsTypeById(id): Observable<SeatsType> {
-    return this.http.get<SeatsType>(`${this.seatsTypesUrl}/${id}`);
+    return this.http.get<SeatsType>(`${SEATS_TYPES_URL}/${id}`);
   }
 
   updateSeatsType(id: string, seatsType: SeatsType) {
-    return this.http.put(`${this.seatsTypesUrl}/${id}`, seatsType, httpOptions);
+    return this.http.put(`${SEATS_TYPES_URL}/${id}`, seatsType, httpOptions);
   }
 
 }
