4e3e2328d6ace198c4dbcaafd2298eb02bb24883
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2018-05-03 11:03:47 +0300

    Fixed user select style

--- src/app/movie-session/movie-session.module.ts
+++ src/app/movie-session/movie-session.module.ts
@@ -15,10 +15,6 @@ import { AdditionsListComponent } from './additions-list/additions-list.componen
 import { AdditionsService } from '../additional-services/additions.service';
 import { MovieSessionService } from './movie-session.service';
 import { MovieSessionSearchBarComponent } from './movie-session-search-bar/movie-session-search-bar.component';
-import { MovieSearchBarComponent } from '../movie/movie-search-bar/movie-search-bar.component';
-import { CinemaSearchBarComponent } from '../cinema/cinema-search-bar/cinema-search-bar.component';
-import {MovieModule} from "../movie/movie.module";
-import {CinemaModule} from "../cinema/cinema.module";
 
 @NgModule({
   imports: [
@@ -27,8 +23,6 @@ import {CinemaModule} from "../cinema/cinema.module";
     FormsModule,
     ReactiveFormsModule,
     CustomFormsModule,
-    MovieModule,
-    CinemaModule,
     BsDatepickerModule.forRoot(),
     TimepickerModule.forRoot(),
     PaginationModule.forRoot()
